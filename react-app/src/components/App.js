4e18a2d9143611445d48c57a5a3a4699d28c886f
Lusaiwen <1811977648@qq.com>
2021-04-14 21:11:48 +0800

    13.ref转发

--- react-app/src/components/App.js
+++ react-app/src/components/App.js
@@ -1,11 +1,26 @@
-import React, { Component } from 'react'
-import Test from './common/Banner/Test'
+import React, { Component } from 'react';
+import WithLog from './common/hoc/WithLog';
+
+class A extends Component {
+    render() {
+        return (<h1>fdsg1</h1>)
+    }
+}
+
+const NewA = WithLog(A);
+
 export default class App extends Component {
+    componentDidMount() {
+        console.log(this.myRef);
+    }
+
+    myRef = React.createRef();
+
     render() {
         return (
             <div>
-                <Test />
+                <NewA ref={this.myRef} />
             </div>
-        )
+        );
     }
 }
