a047b1b4d153e5c2a70a6f28f9dc9f7d112a2a07
Lusaiwen <1811977648@qq.com>
2021-04-15 14:45:08 +0800

    14.context上下文

--- react-app/src/components/App.js
+++ react-app/src/components/App.js
@@ -1,26 +1,58 @@
 import React, { Component } from 'react';
-import WithLog from './common/hoc/WithLog';
 
-class A extends Component {
-    render() {
-        return (<h1>fdsg1</h1>)
-    }
+const ctx = React.createContext();
+
+function ChildA(porps) {
+        return (
+            <>
+                <h1>childA</h1>
+                <ctx.Consumer>
+                    {value => <>{value.a} ,{value.b}</>}
+                </ctx.Consumer>
+                <ChildB />
+            </>
+        );
 }
 
-const NewA = WithLog(A);
-
-export default class App extends Component {
-    componentDidMount() {
-        console.log(this.myRef);
-    }
-
-    myRef = React.createRef();
+class ChildB extends Component {
+    static contextType = ctx;
 
     render() {
         return (
-            <div>
-                <NewA ref={this.myRef} />
-            </div>
+            <>
+                <h1>childB</h1>;
+                <h1>来自上下文a:{this.context.a}  b: {this.context.b}</h1>
+                <button onClick={() => {
+                    this.context.ChangeA(this.context.a + 2)
+                }}>子组件控制a+2</button>
+            </>
+        );
+    }
+}
+
+export default class App extends Component {
+    state = {
+        a: 1234,
+        b: 'abc',
+        ChangeA: newA => {
+            this.setState({
+                a: newA
+            })
+        }
+    };
+    render() {
+        return (
+            <ctx.Provider value={this.state}>
+                <div>
+                    <h1>父组件a:{this.state.a}  b: {this.state.b}</h1>
+                    <ChildA></ChildA>
+                    <button onClick={() => {
+                        this.setState({
+                            a: this.state.a + 1
+                        })
+                    }}>a+1</button>
+                </div>
+            </ctx.Provider>
         );
     }
 }
