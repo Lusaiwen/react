e3d1c32fa6590ce2de52b1d7d5f494cbf68ac8c7
Lusaiwen <1811977648@qq.com>
2021-04-25 20:56:58 +0800

    22.React动画一

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,29 +1,28 @@
-import React from 'react'
-import './myHooks/useAllStudents'
-import usePageStudents from './myHooks/usePageStudents'
+import React, { useState } from 'react';
+import { Transition } from 'react-transition-group';
+import './App.css'
 
+const duration = 300;
 
-function Test(){
-    console.log('运行');
-    const resp = usePageStudents();
-    const lis = resp.findByPage && resp.findByPage.map(it => <li key={it.id}>{it.name}</li>)
-    return (
-        <div>
-            <h1>{resp.cont}</h1>
-            {lis}
-            
-        </div>
-    )
-}
 
 export default function App() {
+    const [inProp, setInProp] = useState(false);
     return (
         <div>
-            <Test/>
+            <Transition in={inProp} timeout={500} addEndListener={(node, done) => {
+                console.log(node, done);
+            }}>
+                {(state) => {
+                    console.log(state);
+                    return (
+                        <div className={state +  ' fetch'}
+                        >
+                            I'm a fade Transition!
+                        </div>
+                    );
+                }}
+            </Transition>
+            <button onClick={() => setInProp(!inProp)}>Click to Enter</button>
         </div>
-    )
+    );
 }
-
-
-
-
