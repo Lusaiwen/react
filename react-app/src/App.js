0809e11567f3dd2992d252d06293a2544e0ccb32
Lusaiwen <1811977648@qq.com>
2021-04-28 22:40:23 +0800

    29.Router-路由信息

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,14 +1,35 @@
 import React from 'react';
-import  { BrowserRouter as Router, Route, Switch} from 'react-router-dom'
-import Login from './pages/Login'
-import Admin from './pages/Admin'
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+
+function News(props) {
+    console.log(props);
+    const year = props.match.params.year;
+    const month = props.match.params.month;
+    const day = props.match.params.day;
+    return (
+        <div>
+            {`当前是${year}年${month}月${day}日`}
+            <button
+                onClick={() => {
+                    props.history.push('/b');
+                }}
+            >
+                跳转B
+            </button>
+        </div>
+    );
+}
+
+function B(props) {
+    return <div>组件B</div>;
+}
 
 export default function App() {
     return (
         <Router>
             <Switch>
-                <Route path="/login" exact component={Login} />
-                <Route path="/" component={Admin} />
+                <Route path="/a/:year(\d+)?/:month?/:day?/*"  exact component={News} />
+                <Route path="/" component={B} />
             </Switch>
         </Router>
     );
