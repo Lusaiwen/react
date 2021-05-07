34fd5967b9d285424614e7e469357e96fbe1d146
Lusaiwen <1811977648@qq.com>
2021-05-07 15:22:18 +0800

    30.Router-其他组件

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,15 +1,54 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import Login from './pages/Login';
-import Admin from './pages/Admin';
+import {
+    BrowserRouter as Router,
+    Route,
+    NavLink,
+    Switch,
+    Redirect
+} from 'react-router-dom';
+
+function A() {
+    return <h1>A页</h1>;
+}
+
+function B() {
+    return <h1>B页</h1>;
+}
+
+function NavBar() {
+    return (
+        <div>
+            <NavLink
+                to={{
+                    pathname: '/a',
+                    hash: '#dsf',
+                    search: '?a=1&b=2',
+                }}
+                innerRef={(node) => {
+                    console.log(node);
+                }}
+                style={{
+                    marginRight: 30,
+                }}
+            >
+                go to A
+            </NavLink>
+            <NavLink to="/b">go to B</NavLink>
+        </div>
+    );
+}
 
 export default function App() {
     return (
-        <Router>
-            <Switch>
-                <Route path="/login" exact component={Login} />
-                <Route path="/" component={Admin} />
-            </Switch>
-        </Router>
+        <div>
+            <Router>
+                <NavBar />
+                <Switch>
+                    <Route path="/a" component={A} />
+                    <Route path="/b" component={B}></Route>
+                    <Redirect to='/a'></Redirect>
+                </Switch>
+            </Router>
+        </div>
     );
 }
