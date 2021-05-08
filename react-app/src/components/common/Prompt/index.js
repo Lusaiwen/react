e1e48b610b01fff26a61798f6da3fcf66caf4f9d
Lusaiwen <1811977648@qq.com>
2021-05-08 20:06:29 +0800

    36.Router-阻止跳转

---
+++ react-app/src/components/common/Prompt/index.js
@@ -0,0 +1,47 @@
+import { Component } from 'react';
+import {withRouter} from 'react-router-dom'
+
+class Prompt extends Component {
+    static defaultProps = {
+        when: false, //当when为true的时候，添加阻塞
+        message: '', //当阻塞时，跳转页面的提示消息
+    };
+
+
+    componentDidMount(){
+        this.handleBlock();
+    }
+
+    componentDidUpdate(prevProps, prevState) {
+        this.handleBlock();
+    }
+    
+    componentWillUnmount(){
+        if(this.unBlock){
+            this.unBlock();
+        }
+    }
+
+
+    handleBlock = () => {
+        if(this.props.when){
+            this.unBlock = this.props.history.block(this.props.message);
+        }else {
+            if(this.unBlock){
+                this.unBlock();
+            }
+        }
+    }
+
+
+
+
+
+
+    render() {
+        return null;
+    }
+}
+
+
+export default withRouter(Prompt);
