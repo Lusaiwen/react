aead1a26b67e5cc562169456776f32932815a797
Lusaiwen <1811977648@qq.com>
2021-05-10 14:38:28 +0800

    37.Router源码-path-to-regexp

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,5 +1,4 @@
 import React from 'react'
-import { BrowserRouter as Router, Route } from 'react-router-dom'
 import "./App.css"
 
 
@@ -7,9 +6,6 @@ import "./App.css"
 export default function App() {
     return (
        <div>
-           {/* <Router>
-               <Route path="/news/123" component={Page1}></Route>
-           </Router> */}
        </div>
     )
 }
