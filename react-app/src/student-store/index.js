0726c9c1d3bc62dbf3a17cf715640877e843cf45
Lusaiwen <1811977648@qq.com>
2021-06-07 14:01:34 +0800

    60.组合-redux和router

--- react-app/src/student-store/index.js
+++ react-app/src/student-store/index.js
@@ -1,13 +1,17 @@
 //创建并导出仓库
 import { createStore, applyMiddleware } from 'redux';
 // import reducer from './reducer失效';
-import reducer from './action'
+import reducer from './action';
 import logger from 'redux-logger';
 import createSagaMiddleWare from 'redux-saga';
 import sagaTask from './saga';
+import { composeWithDevTools } from 'redux-devtools-extension';
 
 const sagaMiddleWare = createSagaMiddleWare();
-const store = createStore(reducer, applyMiddleware(sagaMiddleWare, logger));
+const store = createStore(
+    reducer,
+    composeWithDevTools(applyMiddleware(sagaMiddleWare, logger))
+);
 
 sagaMiddleWare.run(sagaTask);
 console.log(store.getState());
